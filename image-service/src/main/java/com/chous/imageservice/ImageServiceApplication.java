dddd426b703574c4a97927f22881e2ce5a4bfde2
Kidimp <anne.tamaszevicz@gmail.com>
2024-03-21 23:30:16 +0300

    Running containers in Docker is configured 1. Added consul communication settings to each microservice in application.yaml. 2. In each microservice, Dockerfile added and filled out. 3. Created and filled out docker-compose.yaml 4. Added README.md

--- image-service/src/main/java/com/chous/imageservice/ImageServiceApplication.java
+++ image-service/src/main/java/com/chous/imageservice/ImageServiceApplication.java
@@ -2,10 +2,12 @@ package com.chous.imageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ImageServiceApplication {
--- image-service/src/main/java/com/chous/imageservice/config/MongoConfig.java
+++ image-service/src/main/java/com/chous/imageservice/config/MongoConfig.java
@@ -1,14 +1,20 @@
 package com.chous.imageservice.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Collections;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.chous.imageservice")
@@ -17,6 +23,14 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Value("${spring.data.mongodb.database}")
     private String database;
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+    @Value("${spring.data.mongodb.port}")
+    private int port;
 
     @Lazy
     @Autowired
@@ -27,6 +41,14 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Override
     protected String getDatabaseName() {
         return database;
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential(username, database, password.toCharArray()))
+                .applyToClusterSettings(settings -> {
+                    settings.hosts(Collections.singletonList(new ServerAddress(host, port)));
+                });
     }
 
     @Bean
